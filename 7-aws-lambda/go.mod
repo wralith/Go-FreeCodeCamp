2b3f0d8f6fffdf3251a1b3c446570ac118361620
Wralith <wralithdev@gmail.com>
2022-06-08 22:35:02 +0300

    Add Project 7

---
+++ 7-aws-lambda/go.mod
@@ -0,0 +1,5 @@
+module github.com/wralith/go-freecodecamp/7-aws-lambda
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.32.0 // indirect
---
+++ 7-aws-lambda/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+// Deploy ready i guess??
+// Sends json gets json, i guess??
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil
+}
+
+func main() {
+	lambda.Start(HandleLambdaEvent)
+}
